20bc7c68c090a7557a70b3fec0f0b5be0a52e952
nehaks24 <nehagowda604@gmail.com>
2023-11-07 19:06:24 +0530

    Added the first setup files

---
+++ my-js-file.js
@@ -0,0 +1,17 @@
+/*console.log("Hello, this code is from my html file");
+
+const name="neha";
+const year="2003";
+console.log(name);
+console.log(year);*/
+
+const okay=true;
+console.log(okay);
+
+var one=1;
+var two=2.2;
+console.log(one);
+console.log(two);
+
+var num=[1,2,3,4,100,"neha"];
+console.log(num);
